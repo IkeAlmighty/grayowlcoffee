ca34719a468402c96255be60da500fff2535924b
IkeAlmighty <IsaacYates7@gmail.com>
2021-09-14 11:20:47 -0500

    added the authorized tag to wrap the /schedule route

--- pages/schedule/index.js
+++ pages/schedule/index.js
@@ -1,21 +1,19 @@
-import { useSession } from "next-auth/client";
+import Authorized from "../../components/Authorized";
 import Navigation from "../../components/Navigation";
 
 export default function Schedule() {
-  const [session, loading] = useSession();
   return (
     <div>
       <Navigation />
-      {!loading && session && (
-        <>
-          <a className="text-button" href="/schedule/edit">
-            Edit Shedule
-          </a>
-          <a className="text-button" href="/schedule/read">
-            Read Schedule
-          </a>
-        </>
-      )}
+
+      <Authorized>
+        <a className="text-button" href="/schedule/edit">
+          Edit Shedule
+        </a>
+        <a className="text-button" href="/schedule/read">
+          Read Schedule
+        </a>
+      </Authorized>
     </div>
   );
 }
