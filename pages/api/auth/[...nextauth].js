d4808efdb5b3ae24e99220df2f307b3b9a8f414f
IkeAlmighty <IsaacYates7@gmail.com>
2021-09-14 11:19:09 -0500

    jwt callback checks database to see if login is an admin user now

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import { connectToDatabase } from "../../../lib/mongodb";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -14,7 +15,26 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    jwt: async (token, account) => {
+      // if it's my email, then it's an admin:
+      // if (token.email === "isaacyates7@gmail.com") token.isAdmin = true;
 
-  // A database is optional, but required to persist accounts in a database
-  database: process.env.MONGODB_URI,
+      // check if the user is on the database as an admin
+      const { db } = await connectToDatabase();
+      const isAdmin = await db
+        .collection("admin-users")
+        .findOne({ email: token.email });
+
+      // if the user is on the database as an admin, then
+      // set token.isAdmin to true
+      if (isAdmin) token.isAdmin = true;
+
+      return token;
+    },
+    session: async (session, token) => {
+      session.isAdmin = token.isAdmin;
+      return session;
+    },
+  },
 });
