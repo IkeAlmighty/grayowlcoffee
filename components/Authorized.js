8a4ceb9e2dc2074736d120459bbadb4ad8c220c6
IkeAlmighty <IsaacYates7@gmail.com>
2021-09-14 11:17:02 -0500

    created <Authorized> tag for strictly admin components

---
+++ components/Authorized.js
@@ -0,0 +1,28 @@
+import LoginLogout from "./Login";
+import { useSession } from "next-auth/client";
+
+// This component should be used for content that is strictly admin only.
+// Example use:
+// <Authorized>
+//      <div> This is a secret admin dashboard </div>
+// </Authorized>
+export default function Authorized({ children }) {
+  const [session, loading] = useSession();
+
+  if (loading) return <span>Loading...</span>;
+  else {
+    if (session && session.isAdmin) return <>{children}</>;
+    if (session && !session.isAdmin)
+      return (
+        <>
+          <div className="text-center d-block">
+            You are not authorized to view this page.
+          </div>
+          <div className="text-center d-block">
+            Contact Isaac if you are Gray Owl employee and see this message.
+          </div>
+        </>
+      );
+    else return <LoginLogout />;
+  }
+}
