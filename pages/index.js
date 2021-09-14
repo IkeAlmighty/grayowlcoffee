d33404cbb9251601a51b3e036fd513fafa1e3d38
IkeAlmighty <IsaacYates7@gmail.com>
2021-09-14 11:18:35 -0500

    unnessecary import

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "./index.module.css";
 import { signIn, signOut, useSession } from "next-auth/client";
 import Navigation from "../components/Navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const [session, loading] = useSession();
