e06dcbc5f0e69f13a5bc6cb8daf7ff70bf419399
IkeAlmighty <IsaacYates7@gmail.com>
2021-10-06 17:40:18 -0500

    hours widget, location widget:

---
+++ components/home/Location.js
@@ -0,0 +1,7 @@
+export default function Location({ className }) {
+  return (
+    <div className={className}>
+      <a href="https://goo.gl/maps/7zPrPszNxVejJJF99">Directions</a>
+    </div>
+  );
+}
