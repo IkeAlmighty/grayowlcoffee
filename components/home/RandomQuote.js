3613f7aad5e912401eb98d25bf00a40be40ab6e3
IkeAlmighty <IsaacYates7@gmail.com>
2021-10-06 17:39:37 -0500

    random quote widget

---
+++ components/home/RandomQuote.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+export default function QuoteOfDay({ className }) {
+  const [quote, setQuote] = useState("");
+
+  async function getQuote() {
+    let res = await fetch(`/api/db/mongodb?collection=quotes`);
+    let json = await res.json();
+
+    return json[Math.floor(Math.random() * json.length)];
+  }
+
+  useEffect(async () => {
+    setQuote(await getQuote());
+  }, []);
+
+  return (
+    <span className={`${className}`}>
+      {quote ? `${quote.quote}${quote.credit ? " - " + quote.credit : ""}` : ""}
+    </span>
+  );
+}
