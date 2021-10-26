575bdf240f365674618c5d581605786c8239e4ff
IkeAlmighty <IsaacYates7@gmail.com>
2021-10-26 18:55:46 -0500

    removed unused api function

--- pages/api/users.js
+++
@@ -1,9 +0,0 @@
-import { connectToDatabase } from "../../lib/mongodb";
-
-export default async (req, res) => {
-  const { db } = await connectToDatabase();
-
-  const users = await db.collection("users").find({}).limit(20).toArray();
-
-  res.json(users);
-};
