a56fbf8dbeff5ea7d62879bcc54363b0b40be873
Izik <IsaacYates7@gmail.com>
2022-02-15 11:46:35 -0600

    minor css changes, added image to EventCard creation

--- pages/admin/events.js
+++ pages/admin/events.js
@@ -1,10 +1,13 @@
 import { useState, useEffect, useRef } from "react";
 import EventCard from "../../components/EventCard";
+import S3Upload from "../../components/S3Upload";
+import styles from "./events.module.css";
 
 export default function Events() {
   const [details, setDetails] = useState("");
   const [title, setTitle] = useState("");
   const [datetime, setDatetime] = useState("");
+  const [imageKey, setImageKey] = useState("");
 
   const detailsInput = useRef();
   const titleInput = useRef();
@@ -20,7 +23,7 @@ export default function Events() {
 
   async function createEvent(e) {
     e.preventDefault();
-    const event = { details, title, datetime };
+    const event = { details, title, datetime, imageKey };
 
     let res = await fetch("/api/db/events", {
       method: "POST",
@@ -37,6 +40,7 @@ export default function Events() {
       titleInput.current.value = "";
       detailsInput.current.value = "";
       datetimeInput.current.value = "";
+      setImageKey("");
       setDetails("");
       setTitle("");
       setDatetime("");
@@ -57,12 +61,12 @@ export default function Events() {
   }
 
   return (
-    <div className="m-3">
+    <div className="go-container mx-auto my-3 px-3">
       <h2>Publish an Event</h2>
       <hr />
       <form className="container" onSubmit={(e) => createEvent(e)}>
         <div className="row my-3">
-          <label className="col-sm">Title</label>
+          <label className="col-sm my-auto">Title</label>
 
           <input
             ref={titleInput}
@@ -73,7 +77,7 @@ export default function Events() {
         </div>
 
         <div className="row my-3">
-          <label className="col-sm">Details</label>
+          <label className="col-sm my-auto">Details</label>
 
           <textarea
             ref={detailsInput}
@@ -83,42 +87,58 @@ export default function Events() {
         </div>
 
         <div className="row my-3">
-          <label className="col-sm">Date</label>
+          <label className="col-sm my-auto">Date</label>
 
           <input
             ref={datetimeInput}
             className="col-sm"
             type="datetime-local"
             onChange={(e) => {
-              console.log(e.target.value);
               setDatetime(e.target.value);
             }}
+          />
+        </div>
+
+        <div className="row my-3">
+          <label className="col-sm my-auto">Upload an Image: </label>
+
+          <S3Upload
+            className="col-sm"
+            onUpload={(imageKey) => setImageKey(imageKey)}
           />
         </div>
 
         <input type="submit" value="Create Event" />
       </form>
       <h2 className="mt-3">Preview</h2>
-      <EventCard title={title} detailsMarkdown={details} datetime={datetime} />
+      <div className={styles.cardContainer}>
+        <EventCard
+          title={title}
+          detailsMarkdown={details}
+          datetime={datetime}
+          imageKey={imageKey}
+        />
+      </div>
 
       <div style={{ height: "100px" }} />
       <h1 className="mt-3">Events Published</h1>
       <hr />
-      <div>
+      <div className="mx-auto">
         {events.map((event) => (
-          <>
+          <div className={styles.cardContainer}>
             <EventCard
               key={JSON.stringify(event)}
               title={event.title}
               detailsMarkdown={event.details}
               datetime={event.datetime}
+              imageKey={event.imageKey}
             />
             <input
               type="button"
               value="Delete"
               onClick={() => deleteEvent(event)}
             />
-          </>
+          </div>
         ))}
       </div>
     </div>
