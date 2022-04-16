bdec8e919dbb9a42b4d86e9b2bc9688810a0f217
Izik <IsaacYates7@gmail.com>
2022-04-16 12:56:50 -0500

    added link to login on unauthorized message

--- components/Authorized.js
+++ components/Authorized.js
@@ -4,10 +4,20 @@
 //      <div> This is a secret admin dashboard </div>
 // </Authorized>
 import { useClientSideAuth } from "../lib/auth";
+import Link from "next/link";
 export default function Authorized({ children }) {
   const [isAdmin, loading] = useClientSideAuth();
 
   if (loading) return <span>Loading...</span>;
   else if (isAdmin) return <>{children}</>;
-  else return <div>You are not authorized to view this page.</div>;
+  else
+    return (
+      <div>
+        You are not authorized to view this page. If you are a gray owl
+        employee, you can login at &nbsp;
+        <Link href="/login">
+          <a>grayowl.coffee/login</a>
+        </Link>
+      </div>
+    );
 }
