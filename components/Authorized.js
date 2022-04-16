6bf15dcc3f110c6e3ce080cf7e560af57d66f3ff
Izik <IsaacYates7@gmail.com>
2022-04-16 12:15:34 -0500

    refractored to stay in compliance with eslint rules... with an exception

--- components/Authorized.js
+++ components/Authorized.js
@@ -3,9 +3,9 @@
 // <Authorized>
 //      <div> This is a secret admin dashboard </div>
 // </Authorized>
-import { clientSideAuthorized } from "../lib/auth";
+import { useClientSideAuth } from "../lib/auth";
 export default function Authorized({ children }) {
-  const [isAdmin, loading] = clientSideAuthorized();
+  const [isAdmin, loading] = useClientSideAuth();
 
   if (loading) return <span>Loading...</span>;
   else if (isAdmin) return <>{children}</>;
