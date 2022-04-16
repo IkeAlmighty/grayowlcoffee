7f890e1fae5269108e3570e5d3bb19d541ee966f
Izik <IsaacYates7@gmail.com>
2022-04-16 12:48:18 -0500

    comment explains why this page exists at the moment

--- pages/random/index.js
+++ pages/random/index.js
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
 
-export default () => {
+// NOTE: This is here (instead of the config file)
+// for a place to aggregate all the future 'random'
+// pages.
+
+export default function Random() {
   useEffect(() => {
     document.location = "/random/ppalbums";
   }, []);
   return <div></div>;
-};
+}
