f57d4e30b33a206436e84e859182d2be6256dee3
Izik <IsaacYates7@gmail.com>
2022-05-03 13:47:32 -0500

    updated useEffect to prevent race conditions

--- components/home/Spotify.js
+++ components/home/Spotify.js
@@ -2,25 +2,30 @@ import { useEffect, useState } from "react";
 
 export default function Spotify() {
   const [currentSong, setCurrentSong] = useState(undefined);
-  useEffect(async () => {
-    let res = await fetch("/api/spotify/authorize");
-    let token = await res.text();
+  useEffect(() => {
+    //FIXME: this doesn't work for a lot of reasons
+    async function fetchAndSetCurrentSong() {
+      let res = await fetch("/api/spotify/authorize");
+      let token = await res.text();
 
-    console.log(`Bearer ${token}`);
+      console.log(`Bearer ${token}`);
 
-    // call the client side api:
-    let res2 = await fetch(
-      "https://api.spotify.com/v1/124248742/player/currently-playing",
-      {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
+      // call the client side api:
+      let res2 = await fetch(
+        "https://api.spotify.com/v1/124248742/player/currently-playing",
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
-    console.log(await res2.json());
-  }, [currentSong]);
+      console.log(await res2.json());
+    }
+
+    fetchAndSetCurrentSong();
+  }, []);
   return (
     <div>{currentSong ? currentSong : "No music playing right now! :)"}</div>
   );
