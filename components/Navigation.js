e9c48fb955c3ee6fe4ef8421a1f2db1a6973f7e5
ikealmighty <isaacyates7@gmail.com>
2022-07-03 12:54:12 -0500

    updated styling from css module to tailwind, added a signout button that shows when there is a user session

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,26 +1,17 @@
-import { useSession, signOut } from "next-auth/react";
 import { useEffect, useState } from "react";
-import styles from "./Navigation.module.css";
+import Link from "next/link";
 
-export default function Navigation() {
-  const { session, status } = useSession();
-  const [phrase, setPhrase] = useState("");
-
-  useEffect(() => {
-    const phrases = [
-      "Coffee is like the wind. Always in my mouth.",
-      "I think you look great today. Like a fresh latte.",
-      "Crem",
-      "... Someone, let me out of this computer.",
-    ];
-    setPhrase(phrases[Math.floor(Math.random() * phrases.length)]);
-  }, []);
-
+export default function Navigation({ session }) {
   return (
     <div className="header">
-      {/* <img src="/headerimg.png" alt="/!\" className={styles.leftImg} /> */}
-      <span className={styles.headerWords}>Gray Owl Coffee</span>
-      {/* <img src="/headerimg.png" alt="/!\" className={styles.rightImg} /> */}
+      <span className="text-3xl float-left m-3">Gray Owl Coffee</span>
+      {session && (
+        <span className="text-sm m-2 float-right">
+          <Link href="/api/auth/signout">
+            <a>Sign Out</a>
+          </Link>
+        </span>
+      )}
     </div>
   );
 }
